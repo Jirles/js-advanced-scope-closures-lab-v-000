25d68ea8ca001a5dc1ada68ad9ffd57bb1b95258
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 21:46:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function createDriver(){
+  let DriverId = 0;
+  return class {
+    constructor(name){
+      this.id = ++DriverId;
+      this.name = name;
+    }
+  };
+};
+
+function produceDrivingRange(blockRange){
+  return function(startBlock, endBlock){
+    //convert startBlock and endBlock into integers
+    //starting format = '20th', '30th'
+    const delta = Math.abs(parseInt(startBlock.slice(0,2) - parseInt(endBlock.slice(0,2)));
+    if ( delta > blockRange ){
+      return `${delta} blocks out of range`
+    } else {
+      return `within range by ${delta}`
+    }
+  };
+};
